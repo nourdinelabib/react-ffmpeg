981bd4e9f41a9b2bd05bc0a350b023d6c6540902
Nourdine Labib <nourdine.labib.dev@gmail.com>
2023-08-10 09:22:37 +0100

    ffmpeg init

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useState, useRef } from "react";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { toBlobURL } from "@ffmpeg/util";
+
+function App() {
+  const [loaded, setLoaded] = useState(false);
+  const ffmpegRef = useRef(new FFmpeg());
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const messageRef = useRef<HTMLParagraphElement | null>(null);
+
+  const load = async () => {
+    const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.2/dist/esm";
+    const ffmpeg = ffmpegRef.current;
+    ffmpeg.on("log", ({ message }: { message: string }) => {
+      if (messageRef.current) messageRef.current.innerHTML = message;
+    });
+    // toBlobURL is used to bypass CORS issue, urls with the same
+    // domain can be used directly.
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+      wasmURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.wasm`,
+        "application/wasm"
+      ),
+      workerURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.worker.js`,
+        "text/javascript"
+      ),
+    });
+    setLoaded(true);
+    console.log("ffmpeg-core loaded");
+  };
+
+  // const transcode = async () => {
+  //   const videoURL = "https://ffmpegwasm.netlify.app/video/video-15s.avi";
+  //   const ffmpeg = ffmpegRef.current;
+  //   await ffmpeg.writeFile("input.avi", await fetchFile(videoURL));
+  //   await ffmpeg.exec(["-i", "input.avi", "output.mp4"]);
+  //   const data = await ffmpeg.readFile("output.mp4");
+  //   if (videoRef.current)
+  //     videoRef.current.src = URL.createObjectURL(
+  //       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //       // @ts-ignore
+  //       new Blob([data.buffer], { type: "video/mp4" })
+  //     );
+  // };
+
+  return loaded ? (
+    <>
+      <video ref={videoRef} controls></video>
+      <br />
+      {/* <button onClick={transcode}>Transcode avi to mp4</button> */}
+      <p ref={messageRef}></p>
+    </>
+  ) : (
+    <button onClick={load}>Load ffmpeg-core</button>
+  );
+}
+
+export default App;
